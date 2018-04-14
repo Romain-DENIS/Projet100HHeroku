d91a3e5f5d1d5ac2678fb831d224c454348ad2a4
Romain DENIS <romain.denis@hei.yncrea.fr>
2018-04-14 23:49:09 +0200

    test

--- Documents/HEI4/Projet ARENA/ProjetArenaInteliJ/src/main/sql/CreationTableserveur.sql
+++ Documents/HEI4/Projet ARENA/ProjetArenaInteliJ/src/main/sql/CreationTableserveur.sql
@@ -17,13 +17,13 @@ PRIMARY KEY (`pseudo`)
 );
 
 CREATE TABLE Evenement(
-`id` int NOT NULL AUTO_INCREMENT,
+`id` int NOT NULL,
 `nomE` VARCHAR(50) NOT NULL,
 `descri` VARCHAR(500) NOT NULL,
 `dateE` date NOT NULL,
 `plateforme` VARCHAR(50) NOT NULL,
 `interhei` bool NOT NULL,
-`payant` bool NOT NULL,
+`payant` int NOT NULL,
 PRIMARY KEY (`id`)
 );
 
@@ -48,12 +48,17 @@ ON UPDATE CASCADE;
 CREATE TABLE Commentaire(
   `commentaire` TEXT NOT NULL,
   `pseudo` VARCHAR(50) NOT NULL,
-  `idcommentaire` int NOT NULL AUTO_INCREMENT,
+
   `idevut` int not null ,
   FOREIGN KEY (`idevut`) REFERENCES Evenement(id),
   FOREIGN KEY (`pseudo`) REFERENCES Utilisateur(pseudo),
-  primary key(`idcommentaire`)
+  primary key(`idevut`,`pseudo`)
 );
 
 INSERT INTO utilisateur(`nom`,`prenom`,`pseudo`,`mot_de_passe`,`email`,`classe`,`notif`) VALUES ('Admin','Admin','Administrateur','ARENAHEI','.@hei.yncrea.fr','H',0);
+
+
+INSERT INTO utilisateur(`nom`,`prenom`,`pseudo`,`mot_de_passe`,`email`,`classe`,`notif`) VALUES ('nom1','prenom1','pseudo1','mdp1','test1@exemple','H44',1);
+INSERT INTO utilisateur(`nom`,`prenom`,`pseudo`,`mot_de_passe`,`email`,`classe`,`notif`) VALUES ('nom2','prenom2','pseudo2','mdp2','test2@exemple','H42',0);
+INSERT INTO utilisateur(`nom`,`prenom`,`pseudo`,`mot_de_passe`,`email`,`classe`,`notif`) VALUES ('nom3','prenom3','pseudo3','mdp3','test3@exemple','H43',1);
 
